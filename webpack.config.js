c4d38e3de644b0c35a961526e28bf4177db19f36
amogelang chaane <amogelangchaane200@gmail.com>
2023-03-30 16:48:34 +0200

    added responsiveness

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,17 @@ module.exports = {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
         },
+        {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer
+                ]
+              }
+            }
+        }
     ],
 },
 };
